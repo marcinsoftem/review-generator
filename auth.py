7c721d60acdddafb2c4f0dd4be217353c71ae598
Marcin Mikłas <m.miklas@wszib.edu.pl>
2024-06-24 09:46:25 +0200

    v0.0.7

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ def key(key : str):
         with st.form("key_form"):
             st.session_state["auth_key"] = st.text_input("Podaj kod, aby skorzystć z aplikacji")
             if st.form_submit_button("Zaloguj"):
-                if st.session_state["auth_key"] != st.secrets["KEY"]:
+                if st.session_state["auth_key"] != key:
                     st.warning("Kod jest nieprawidłowy")
                 else:
                     st.experimental_rerun()
